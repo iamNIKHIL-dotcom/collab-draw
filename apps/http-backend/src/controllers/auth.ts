acbb1c5c74f82bbd20809fccf00ce3a62bf9494c
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-05-17 22:14:56 +0530

    db,auth

---
+++ apps/http-backend/src/controllers/auth.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+export const signup = async (req:Request, res: Response) => {
+    
+}
+export const signin = async (req:Request, res: Response) => {
+    
+}
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import authRouter from "./routes/auth";
 
 const app = express();
 app.use(express.json());
+
+
+app.use("/api/v1/auth", authRouter);
+
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");

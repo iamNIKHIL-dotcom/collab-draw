cba7bd008a7cd9bfdbe2f62c8f332e32e517c3c4
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-05-25 23:18:39 +0530

    mouseUp

--- apps/web/app/draw/Canvas.ts
+++ apps/web/app/draw/Canvas.ts
@@ -85,7 +85,7 @@ export class Canvas {
       this.ctx.lineWidth = this.LINE_WIDTH;
 
       if(shape.type === "rect"){
-
+        this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
       }
       else if(shape.type === "circle"){
 
@@ -100,10 +100,95 @@ export class Canvas {
 
   }
   initHandlers(){
+    this.socket.onmessage = (event) =>{
+      try{
+        const message = JSON.parse(event.data);
+        if(message.type === "shape_update"){
+          const parsedShape = JSON.parse(message.message);
+          this.existingShapes.push(parsedShape);
+          this.redraw();
+        }
+      }catch(error){
+        console.log("error handline webSocket message:", error);
+      }
+    }
 
   }
+
+  redraw(){
+    this.clearCanvas();
+
+      this.ctx.setTransform(
+      this.scale,
+      0,
+      0,
+      this.scale,
+      this.offsetX,
+      this.offsetY
+    );
+
+    this.drawExistingShapes();
+  }
+
+  clearCanvas(){
+    this.ctx.save();
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0); // Reset transform
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.restore();    
+  }
   initMouseHandlers(){
+    this.canvas.addEventListener("mousedown", this.mouseDownHandler);
+    this.canvas.addEventListener("mouseup", this.mouseUpHandler);
+    this.canvas.addEventListener("mousemove", this.mouseMoveHandler);
+  }
+  mouseDownHandler = (e :MouseEvent) =>{
+    if(this.selectedTool === "pan" || e.buttons === 2 || e.button === 2){
+
+    }
+    if(this.selectedTool === "pencil"){
+
+    }
+
+  }
+  mouseUpHandler = (e : MouseEvent){
+    if(this.isPanning){
+      this.isPanning = false;
+      this.lastPanPoint = null;
+
+      if(this.selectedTool == "pan"){
+        this.canvas.style.cursor = "grab";
+      }
+      return;
+    }
+
+    this.clicked = false;
     
+    // Transform mouse coordinates
+    const endX = (e.clientX - this.offsetX) / this.scale;
+    const endY = (e.clientY - this.offsetY) / this.scale;
+
+    if (this.selectedTool === "pencil") {
+      const shape: Shape = {
+        type: "pencil",
+        points: this.pencilPoints,
+      };
+      this.existingShapes.push(shape);
+      this.socket.send(
+        JSON.stringify({
+          type: "shape_update",
+          message: JSON.stringify(shape),
+          roomId: Number(this.roomId),
+        })
+      );
+      this.pencilPoints = [];
+    }
+
+  } 
+
+  mouseMoveHandler = (e : MouseEvent){
+
   }
 
   destroy() {}
